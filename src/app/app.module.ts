5facfa05294bddb7bf766efb98c6e155e6118f04
Anthony <anthonymarques@outlook.com>
2020-04-25 20:51:37 +0200

    updated angular version to version 9 trying to fix compile errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AngularFireModule } from '@angular/fire';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AngularFireModule } from "@angular/fire";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
 
-import { AppComponent } from './app.component';
-import { EmployeeComponent } from './employees/employee/employee.component';
-import { EmployeeListComponent } from './employees/employee-list/employee-list.component';
-import { EmployeesComponent } from './employees/employees.component';
-import { environment } from 'src/environments/environment';
+import { AppComponent } from "./app.component";
+import { EmployeeComponent } from "./employees/employee/employee.component";
+import { EmployeeListComponent } from "./employees/employee-list/employee-list.component";
+import { EmployeesComponent } from "./employees/employees.component";
+import { environment } from "src/environments/environment";
+import { EmployeesService } from "./shared/employees.service";
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeeComponent,
     EmployeeListComponent,
-    EmployeesComponent
+    EmployeesComponent,
   ],
   imports: [
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFirestoreModule,
     BrowserModule,
-
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [EmployeesService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/shared/employees.service.ts
+++ src/app/shared/employees.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
+import { Employee } from './employee.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeesService {
-
+ formData: Employee;
   constructor() { }
 }
