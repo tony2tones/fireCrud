17d9151085dfa19a76913fd349b448ff16ef027d
Anthony <anthonymarques@outlook.com>
2020-04-26 19:20:54 +0200

    adding form data

--- src/app/employees/employee/employee.component.ts
+++ src/app/employees/employee/employee.component.ts
@@ -1,16 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { EmployeesService } from 'src/app/shared/employees.service';
+import { Component, OnInit } from "@angular/core";
+import { EmployeesService } from "src/app/shared/employees.service";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-employee',
-  templateUrl: './employee.component.html',
-  styleUrls: ['./employee.component.css']
+  selector: "app-employee",
+  templateUrl: "./employee.component.html",
+  styleUrls: ["./employee.component.css"],
 })
 export class EmployeeComponent implements OnInit {
-
-  constructor(private employeeService: EmployeesService) { }
+  constructor(private employeeService: EmployeesService) {}
 
   ngOnInit() {
+    this.resetForm();
   }
 
+  resetForm(form?: NgForm) {
+    if (form != null) 
+    form.resetForm();
+    this.employeeService.formData = {
+      id: null,
+      fullName: "",
+      position: "",
+      empCode: "",
+      mobile: ""
+    };
+  }
 }
