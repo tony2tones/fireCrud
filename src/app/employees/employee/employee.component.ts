26e56a33a5d2099b5637f3f765cc5271bd4fc74a
Anthony <anthonymarques@outlook.com>
2020-04-27 14:42:59 +0200

    basic form and first post to firebase

--- src/app/employees/employee/employee.component.ts
+++ src/app/employees/employee/employee.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { EmployeesService } from "src/app/shared/employees.service";
 import { NgForm } from "@angular/forms";
+import { AngularFirestore } from "@angular/fire/firestore";
 
 @Component({
   selector: "app-employee",
@@ -8,21 +9,29 @@ import { NgForm } from "@angular/forms";
   styleUrls: ["./employee.component.css"],
 })
 export class EmployeeComponent implements OnInit {
-  constructor(private employeeService: EmployeesService) {}
+  constructor(
+    private employeeService: EmployeesService,
+    private firestore: AngularFirestore
+  ) {}
 
   ngOnInit() {
     this.resetForm();
   }
 
   resetForm(form?: NgForm) {
-    if (form != null) 
-    form.resetForm();
+    if (form != null) form.resetForm();
     this.employeeService.formData = {
       id: null,
       fullName: "",
       position: "",
       empCode: "",
-      mobile: ""
+      mobile: "",
     };
+  }
+
+  onSubmit(form: NgForm) {
+    let data = form.value;
+    this.firestore.collection('employees').add(data);
+    this.resetForm(form);
   }
 }
