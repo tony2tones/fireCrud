e312307f47d713dd9e6164fc4fe50a27cd54d389
Anthony <anthonymarques@outlook.com>
2020-04-27 16:29:14 +0200

    adding service to get employee data

--- src/app/employees/employee-list/employee-list.component.ts
+++ src/app/employees/employee-list/employee-list.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { EmployeesService } from "src/app/shared/employees.service";
+import { Employee } from "src/app/shared/employee.model";
+import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
 
 @Component({
-  selector: 'app-employee-list',
-  templateUrl: './employee-list.component.html',
-  styleUrls: ['./employee-list.component.css']
+  selector: "app-employee-list",
+  templateUrl: "./employee-list.component.html",
+  styleUrls: ["./employee-list.component.css"],
 })
 export class EmployeeListComponent implements OnInit {
-
-  constructor() { }
+  list: Employee[];
+  constructor(private service: EmployeesService) {}
 
   ngOnInit() {
+    this.service.getEmployee().subscribe((actionArray) => {
+      this.list = actionArray.map(item => {
+        return {
+          id: item.payload.doc.id,
+          ...item.payload.doc.data()
+        } as Employee;
+      });
+    });
   }
-
 }
--- src/app/shared/employees.service.ts
+++ src/app/shared/employees.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from "@angular/core";
 import { Employee } from "./employee.model";
+import { AngularFirestore } from '@angular/fire/firestore/firestore';
 
 @Injectable({
   providedIn: "root",
 })
 export class EmployeesService {
   formData: Employee;
-  constructor() {}
+
+  constructor(private firestore: AngularFirestore) {}
+
+  getEmployee() {
+    return this.firestore.collection('employees').snapshotChanges();
+  }
+
 }
